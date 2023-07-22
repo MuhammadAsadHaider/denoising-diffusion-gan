639d268093c98524ca25ea0d28c3a6d3994bcdb9
DESKTOP-A45213N\Asad <m.asadh@outlook.com>
2023-07-22 19:38:59 -0400

    fix

--- data_loaders/humanml/utils/get_opt.py
+++ data_loaders/humanml/utils/get_opt.py
@@ -70,7 +70,7 @@ def get_opt(opt_path, device):
         opt.max_motion_length = 196
     elif opt.dataset_name == 'h2s':
         opt.data_root = './dataset/H2S'
-        opt.motion_dir = pjoin(opt.data_root, 'new_joint_vecs')
+        opt.motion_dir = pjoin(opt.data_root, 'features')
         opt.text_dir = pjoin(opt.data_root, 'texts')
         opt.joints_num = 75
         opt.dim_pose = 225
